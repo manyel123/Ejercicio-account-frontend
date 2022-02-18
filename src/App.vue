c4cedca097d724bd6c2897e499c87f08a4ffd965
Miguel Angel Pico <miguelanngel@hotmail.es>
2022-02-18 11:24:40 -0500

    completed login signup and account

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
     <div id="app" class="app">
-
         <div class="header">
             <h1>App</h1>
+            <nav>
+                <button v-if="is_auth"   v-on:click="loadHome">
+                    Inicio</button>
+                <button v-if="is_auth"   v-on:click="loadAccount">
+                    Cuenta</button>
+                <button v-if="is_auth"   v-on:click="logOut">
+                    Cerrar Sesión</button>
+                <button v-if="!is_auth"  v-on:click="loadLogIn">
+                    Iniciar Sesión</button>
+                <button v-if="!is_auth"  v-on:click="loadSignUp">
+                    Registrarse
+                </button>
+            </nav>
         </div>
 
+        <div class="main-component">
+            <router-view
+                v-on:completedLogIn="completedLogIn"
+                v-on:completedSignUp="completedSignUp"
+                v-on:logOut="logOut"
+            >
+            </router-view>
+        </div>
+
+        <div class="footer">
+            <h2>App footer 2022</h2>
+        </div>
     </div>
 </template>
 
@@ -12,16 +36,71 @@
 export default {
     name: "App",
 
-    data: function () {},
+    data: function () {
+        /*Variables o información que se cargarán desde el inicio */
+        return {
+            is_auth: false,
+        };
+    },
 
-    methods: {},
+    components: {},
 
-    created: function () {},
+    methods: {
+        /*métodos javaS del componente*/
+        verifyAuth: function () {
+            this.is_auth = localStorage.getItem("isAuth") || false;
+
+            if (this.is_auth == false) {
+                this.$router.push({ name: "logIn" });
+            } else this.$router.push({ name: "home" });
+        },
+
+        loadLogIn: function () {
+            this.$router.push({ name: "logIn" });
+        },
+
+        loadSignUp: function () {
+            this.$router.push({ name: "signUp" });
+        },
+
+        completedLogIn: function (data) {
+            localStorage.setItem("isAuth", true);
+            localStorage.setItem("username", data.username);
+            localStorage.setItem("token_access", data.token_access);
+            localStorage.setItem("token_refresh", data.token_refresh);
+            alert("Autenticación Exitosa");
+            this.verifyAuth();
+        },
+
+        completedSignUp: function (data) {
+            alert("Registro Exitoso");
+            this.completedLogIn(data);
+        },
+
+        loadHome: function () {
+            this.$router.push({ name: "home" });
+        },
+
+        loadAccount: function () {
+            this.$router.push({ name: "account" });
+        },
+
+        logOut: function () {
+            localStorage.clear();
+            alert("Sesión Cerrada");
+            this.verifyAuth();
+        },
+    },
+
+    created: function () {
+        /*Funciones que se ejecutan al cargar el componente*/
+        this.verifyAuth();
+    },
 };
 </script>
 
 <style>
-body{
+body {
     margin: 0 0 0 0;
 }
 
@@ -31,7 +110,7 @@ body{
     width: 100%;
     height: 10vh;
     min-height: 100px;
-    
+
     background-color: #283747;
     color: #e5e7e9;
     display: flex;
@@ -39,8 +118,56 @@ body{
     align-items: center;
 }
 
-.header h1{
+.header h1 {
     width: 20%;
     text-align: center;
+}
+
+.header nav {
+    height: 100%;
+    width: 20%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 20px;
+}
+
+.header nav button {
+    color: #e5e7e9;
+    background: #283747;
+    border: 1px solid #e5e7e9;
+    border-radius: 5px;
+    padding: 10px 20px;
+}
+
+.header nav button:hover {
+    color: #283747;
+    background: #e5e7e9;
+    border: 1px solid #e5e7e9;
+}
+
+.main-component {
+    height: 75vh;
+    margin: 0%;
+    padding: 0%;
+    background: #fdfefe;
+}
+
+.footer {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    background-color: #283747;
+    color: #e5e7e9;
+}
+
+.footer h2 {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,38 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
+import Login   from './components/Login.vue'
+import SignUp  from './components/SignUp.vue'
+import Home    from './components/Home.vue'
+import Account from './components/Account.vue'
+
 const routes = [
   {
     path: '/',
     name: 'root',
     component: App
   },
-]
+  {
+    path: '/user/login',
+    name: 'logIn',
+    component: Login
+  },
+  {
+    path: '/user/SignUp',
+    name: 'signUp',
+    component: SignUp
+  },
+  {
+    path: '/user/home',
+    name: "home",
+    component: Home
+  },
+  {
+    path: '/user/account',
+    name: "account",
+    component: Account
+  }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
